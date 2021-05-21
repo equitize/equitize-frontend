a59beac5f30b317e55c1605b62340293cbf8accd
David Fan <fan.david@dhs.sg>
2021-05-21 08:46:57 +0800

    Dynamic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InvalidPage from "./containers/invalidPage/InvalidPage";
 import Header from "./components/Header/Header";
 import {
@@ -7,12 +7,33 @@ import {
     Switch,
 } from "react-router-dom";
 import Homepage from './components/Homepage';
+import Dropdown from './components/Dropdown';
 
 
 function App() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    useEffect(() => {
+        const hideMenu = () => {
+                setIsOpen(false)
+            }
+        }
+
+        window.addEventListener('resize', hideMenu)
+
+        return () => {
+            window.removeEventListener('resize', hideMenu)
+        }
+    })
+
   return (
       <Router>
-          <Header />
+          <Header toggle={toggle}/>
+          <Dropdown isOpen={isOpen} toggle={toggle}/>
           <Homepage />
 
           <Switch>
@@ -26,7 +47,5 @@ function App() {
       </Router>
   );
 }
-
-
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,32 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'; // ES6
 
-const Header = () => {
+const Header = ({ toggle }) => {
     return (
         <nav className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono' role='navigation'>
             <Link to='/' className='pl-8'>Equitize</Link>
-            <div className="px-4 cursor-pointer md:hidden">
+            <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
             </div>
             <div className="pr-8 md:block hidden">
-                <Link className="p-4" to='/'>Home</Link>
                 <Link className="p-4" to='/about'>About</Link>
                 <Link className="p-4" to='/contact'>Contact</Link>
                 <Link className="p-4" to='/keyFeatures'>Key Features</Link>
+                <Link className="p-4 font-bold" to='/login'>
+                    Login
+                </Link>
+                <Link className="p-4 font-bold bg-secondary hover:bg-blue-700 text-white py-2 px-5 rounded-full" to='/register'>
+                    Register
+                </Link>
             </div>
         </nav>
 
     )
+}
+
+Header.propTypes = {
+    toggle: PropTypes.func
 }
 
 export default Header
