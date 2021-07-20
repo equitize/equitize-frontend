f0f9fb0179d2eed4f644aae053a1631eb1c80796
hkmac <tonghuikang@outlook.com>
2021-07-21 01:40:59 +0800

    retail investor tests

--- src/tests/startup-signup.test.js
+++ src/tests/startup-signup.test.js
@@ -24,7 +24,7 @@ it('Testing to see if frontpage works', async () => {
 
     await driver.get("http://localhost:3000");
     await sleep(3000)
-        
+
     var title = await driver.getTitle();
     console.log('Title is:',title);
     expect(title).toContain("Equitize")
@@ -32,108 +32,122 @@ it('Testing to see if frontpage works', async () => {
     await driver.findElement(By.partialLinkText("Register")).click();
     await sleep(3000)
 
-    await driver.findElement(By.css("img[alt='Register as Startup']")).click();
+    await driver.findElement(By.css('img[alt="Register as Startup"]')).click();
+    await sleep(500)
+
+    await driver.findElement(By.css('input[placeholder="Company Name"]')).sendKeys(companyName);
+    await sleep(500)
+
+    await driver.findElement(By.css('input[placeholder="Email Address"]')).sendKeys(emailAddress);
+    await sleep(500)
+
+    await driver.findElement(By.css('input[placeholder="Password"]')).sendKeys(password);
     await sleep(1000)
 
-    await driver.findElement(By.css("input[placeholder='Company Name']")).sendKeys(companyName);
-    await sleep(1000)
-
-    await driver.findElement(By.css("input[placeholder='Email Address']")).sendKeys(emailAddress);
-    await sleep(1000)
-
-    await driver.findElement(By.css("input[placeholder='Password']")).sendKeys(password);
+    await driver.findElement(By.xpath("//button[contains(text(),'" + "Sign Up" +"')]")).click();
     await sleep(2000)
 
-    for (let attemptCount in [...Array(2).keys()]){
-        try {
-            // await driver.findElement(By.partialLinkText("Sign Up")).click();
-            // await sleep(2000)
-            await driver.findElement(By.css("input[placeholder='Company Name']")).sendKeys(Key.ENTER);
-            await sleep(1000)
-            await driver.findElement(By.css("input[placeholder='Email Address']")).sendKeys(Key.ENTER);
-            await sleep(1000)
-            await driver.findElement(By.css("input[placeholder='Password']")).sendKeys(Key.ENTER);
-            await sleep(1000)
-          } catch {
-          continue
-        }
-        break
-    }
-
     await sleep(5000)
-    let elements = await driver.findElements(By.className("bg-custom-gray"));
 
+    let elements = await driver.findElements(By.className("bg-custom-gray"));
     for (let idx in elements){
         await elements[idx].click();
         await sleep(2000)
-        await driver.findElement(By.className("test-confirm")).click();
+        await driver.findElement(By.xpath("//button[contains(text(),'" + "Confirm" +"')]")).click();
         await sleep(2000)
     }
 
     await driver.findElement(By.partialLinkText("Submit")).click();
     await sleep(2000)
 
+    // token faulty, await new version
+    // await driver.findElement(By.css("input[id='capTable']")).sendKeys(sample_pdf_path);
+    // await sleep(1000)
 
-    await driver.findElement(By.css("input[id='capTable']")).sendKeys(sample_pdf_path);
-    await sleep(1000)
+    // await driver.findElement(By.css("input[id='acraDocuments']")).sendKeys(sample_pdf_path);
+    // await sleep(1000)
 
-    await driver.findElement(By.css("input[id='acraDocuments']")).sendKeys(sample_pdf_path);
-    await sleep(1000)
+    // await driver.findElement(By.css("input[id='bankInfo']")).sendKeys(sample_pdf_path);
+    // await sleep(1000)
 
-    await driver.findElement(By.css("input[id='bankInfo']")).sendKeys(sample_pdf_path);
-    await sleep(1000)
+    // await driver.findElement(By.css("input[id='idProof']")).sendKeys(sample_pdf_path);
+    // await sleep(1000)
 
-    await driver.findElement(By.css("input[id='idProof']")).sendKeys(sample_pdf_path);
-    await sleep(1000)
-
-    await driver.findElement(By.css("input[id='profilePhoto']")).sendKeys(sample_jpeg_path);
-    await sleep(1000)
+    // await driver.findElement(By.css("input[id='profilePhoto']")).sendKeys(sample_jpeg_path);
+    // await sleep(1000)
 
     await driver.findElement(By.css("textarea[placeholder='Zil address']")).sendKeys(sample_input_string);
-    await sleep(2000)
+    await sleep(500)
 
     await driver.findElement(By.css("textarea[placeholder='Short Description of your Business']")).sendKeys(sample_input_string);
+    await sleep(500)
+
+    await driver.findElement(By.css('input[type="search"]')).click();
+    await sleep(1000)
+    await driver.findElement(By.css('svg[fill="currentColor"]')).click();
+    await sleep(1000)
+    await driver.findElement(By.css("textarea[placeholder='Zil address']")).click();
+    await sleep(1000)
+
+    // campaign setup tab
+
+    // input[accept="video/*"]
+    // input[accept=".jpg, .pdf"]
+    await driver.findElement(By.xpath("//button[contains(text(),'" + "Campaign Setup" +"')]")).click();
     await sleep(2000)
-
-    // await driver.findElement(By.css("input[type='search']")).click();
-    // await sleep(2000)
-
-    // not interactable 
-    // await driver.findElement(By.css("svg[stroke='currentColor']")).findElement(By.css("path")).click();
-    // await sleep(2000)
-
-    await driver.findElement(By.className("self-end bg-secondary")).click();
-    await sleep(2000)
-
-    await driver.findElement(By.className("bg-transparent text-black h-6 w-6")).click();
+    await driver.findElement(By.css("textarea[placeholder='Campaign Description']")).sendKeys(sample_input_string);
     await sleep(2000)
 
     
-    await driver.findElement(By.xpath("//button[contains(text(),'" + "Campaign Setup" +"')]")).click();
+    await driver.findElement(By.xpath('//p[contains(text(),"SET UP PROPOSED CAMPAIGN MILESTONES")]')).click()
+    await sleep(1000)
+
+    await driver.findElement(By.css('img[alt="Edit Details"]')).click();
+    await sleep(500)
+    await driver.findElement(By.css('input[value="500000"]')).sendKeys(Key.UP);
+    await sleep(500)
+    await driver.findElement(By.css('input[value="10"]')).sendKeys(Key.BACK_SPACE);
+    await sleep(500)
+    await driver.findElement(By.css('input[value="10"]')).sendKeys("5");
+    await sleep(500)
+    await driver.findElement(By.css('input[value="10"]')).sendKeys(Key.ENTER);
+    await sleep(500)
+    await driver.findElement(By.xpath('//button[contains(text(),"Submit")]')).click()
+    await sleep(1000)
+
+    await driver.findElement(By.xpath('//p[contains(text(),"+ ADD NEW MILESTONE")]')).click()
+    await sleep(500)
+    await driver.findElement(By.css('input[placeholder="Milestone Title"]')).sendKeys(sample_input_string);
+    await sleep(500)
+    await driver.findElement(By.css('input[type="Date"]')).sendKeys("09-09-2021");
+    await sleep(500)
+    await driver.findElement(By.css('textarea[placeholder="Description"]')).sendKeys(sample_input_string);
+    await sleep(500)
+    await driver.findElement(By.xpath('//button[contains(text(),"Submit")]')).click()
+    await sleep(1000)
+
+    await driver.findElement(By.xpath('//p[contains(text(),"×")]')).click()
+    await sleep(1000)
+
+    await driver.findElement(By.xpath('//p[contains(text(),"ZOOM")]')).click()
+    await sleep(1000)
+    // TBC    
+    await driver.findElement(By.xpath('//button[contains(text(),"Submit")]')).click()
+    await sleep(1000)
+    
+    // search for commerical champion tab
+    await driver.findElement(By.xpath("//button[contains(text(),'" + "Search for Commercial Champion" +"')]")).click();
+    await sleep(2000)
+    await driver.findElement(By.css('img[alt="Not Selected"]')).click();
     await sleep(2000)
 
-    // await driver.findElement(By.css("input[id='capTable']")).sendKeys("/Users/hkmac/Desktop/equitize-frontend/src/tests/sample_files/sample.jpeg");
-    // await sleep(2000)
-    // await 
-    // await sleep(2000)
+    // apply for design sprints tab
+    await driver.findElement(By.xpath("//button[contains(text(),'" + "Apply for Design Sprints" +"')]")).click();
+    await sleep(2000)
+    await driver.findElement(By.xpath("//p[contains(text(),'" + "15th May" +"')]")).click();
+    await sleep(2000)
 
-    // 
-    // `${__dirname}/../src/tests/sample_files/sample.jpeg`
-
-    // await driver.findElement(By.linkText("Confirm")).click();
-    // await sleep(2000)
-
-    // await driver.findElement(By.partialLinkText("Campaign Process Guide")).click();
-    // await sleep(2000)
-
-    // await driver.findElement(By.linkText("Confirm")).click();
-    // await sleep(2000)
-
-    // await driver.findElement(By.partialLinkText("Terms and Conditions")).click();
-    // await sleep(2000)
-
-    // await driver.findElement(By.linkText("Confirm")).click();
-    await sleep(200000)
+    await sleep(20000)
 
     //It is always a safe practice to quit the browser after execution
     await driver.quit();
