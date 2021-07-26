812522c390f102cbe8c246f11a5b37646abbe484
Cre8tion <ljl1997@gmail.com>
2021-07-26 17:20:22 +0800

    Closed #5

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+const SERVER_URL = process.env.NODE_ENV === "production" ? "https://api.equitize.xyz/api" : "http://localhost:8080/api"
+
 const ConfigData = {
-    "SERVER_URL": "http://localhost:8080/api",
+    "SERVER_URL": SERVER_URL,
     "AUTH_URL": "https://dev-cus1s5do.au.auth0.com",
     "JWKS_CLIENT": "https://dev-cus1s5do.au.auth0.com/.well-known/jwks.json"
 }
